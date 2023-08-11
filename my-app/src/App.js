eca40731016d75cb6b38fe7ce094ae862d3896f9
Merve Doğan <merve36540@gmail.com>
2023-08-11 14:31:34 +0300

    millionaire quiz app is done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,11 @@
 import { useEffect, useMemo, useState } from 'react';
 import './index.css';
 import Timer from './Components/Timer';
-import Game from './Components/Timer';
+import Game from './Components/Game';
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
   const [timeOver, setTimeOver] = useState(false);
-  const [earnedMoney, setEarnedMoney]=useState("$ 0");
-  const moneyTree = useMemo(
-    () =>
-      [
-        { questionNo: 1, amount: " $ 500" },
-        { questionNo: 2, amount: " $ 1.000" },
-        { questionNo: 3, amount: " $ 2.000" },
-        { questionNo: 4, amount: " $ 3.000" },
-        { questionNo: 5, amount: " $ 5.000" },
-        { questionNo: 6, amount: " $ 7.500" },
-        { questionNo: 7, amount: " $ 15.000" },
-        { questionNo: 8, amount: " $ 30.000" },
-        { questionNo: 9, amount: " $ 60.000" },
-        { questionNo: 10, amount: " $ 125.000" },
-        { questionNo: 11, amount: " $ 250.000" },
-        { questionNo: 12, amount: " $ 1.000.000" }
-
-      ].reverse(), []
-  ); 
-  useEffect(()=>{
-    questionNumber>1 && setEarnedMoney(moneyTree.find((m) => m.id === questionNumber - 1).amount);
-  }, [questionNumber, moneyTree]);
-
+  const [earnedMoney, setEarnedMoney] = useState("$ 0");
   const questionList = [
     {
       questionNo: 1,
@@ -295,19 +273,56 @@ function App() {
     }
 
   ]
+  const moneyTree = useMemo(
+    () =>
+      [
+        { questionNo: 1, amount: " $ 500" },
+        { questionNo: 2, amount: " $ 1.000" },
+        { questionNo: 3, amount: " $ 2.000" },
+        { questionNo: 4, amount: " $ 3.000" },
+        { questionNo: 5, amount: " $ 5.000" },
+        { questionNo: 6, amount: " $ 7.500" },
+        { questionNo: 7, amount: " $ 15.000" },
+        { questionNo: 8, amount: " $ 30.000" },
+        { questionNo: 9, amount: " $ 60.000" },
+        { questionNo: 10, amount: " $ 125.000" },
+        { questionNo: 11, amount: " $ 250.000" },
+        { questionNo: 12, amount: " $ 1.000.000" }
+
+      ].reverse(), []
+  );
+  useEffect(() => {
+    questionNumber > 1 && setEarnedMoney(moneyTree.find((m) => m.questionNo === questionNumber - 1).amount);
+  }, [questionNumber, moneyTree]);
+
+
   return (
     <div className="App">
-      <div className='MainStage'>Stage </div>
-      <div className='Timer'>
-        <Timer setTimeOver={setTimeOver}
-          questionNumber={questionNumber} />
+      <div className='MainStage'>
+      {timeOver ? (
+              <h1 className="endText">You earned: {earnedMoney}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="Timer">
+                    <Timer
+                      setTimeOver={setTimeOver}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Game
+                    questionList={questionList}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOver={setTimeOver}
+                  />
+                </div>
+              </>
+            )}
       </div>
-      <div className='seeQuestion'>
-        <Game questionList={questionList}  
-              questionNumber={questionNumber}
-              setQuestionNumber={setQuestionNumber}
-              setTimeOver={setTimeOver} />
-        </div> 
+
       <div className='Questions'>
         <ul className='Tree' >
           {moneyTree.map((m) => (
--- my-app/src/Components/Game.jsx
+++ my-app/src/Components/Game.jsx
@@ -1,14 +1,79 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import play from "../musics/play.mp3";
+import correct from "../musics/correct.mp3";
+import wrong from "../musics/wrong.mp3";
 export default function Game({
-    questionList,
-    questionNumber,
-    setQuestionNumber,
-    setTimeOver
+  questionList,
+  questionNumber,
+  setQuestionNumber,
+  setTimeOver,
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
-})
-const [question,setQuestion]= useState(null);
-const[selectedAnswer, setSelectedAnswer]=useState(null);
-const [answerStatus, setAnswerStatus]=useState("answer");
- const handleClick =(a) => {
-    
- }
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+  useEffect(() => {
+    setQuestion(questionList[questionNumber - 1]);
+  }, [questionList, questionNumber]);
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () => {
+      setClassName(a.correct ? "answer correct" : "answer wrong");
+    });
+
+    delay(3000, () => {
+      if (a.correct) {
+        correctAnswer();
+        delay(4000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
+      } else {
+        wrongAnswer();
+        setClassName("answerrwrong");
+        const correctAnswerIndex = question.answers.findIndex(
+          (ans) => ans.correct
+        );
+
+        delay(2000, () => {
+          setClassName("answerrcorrect");
+          setSelectedAnswer(question.answers[correctAnswerIndex]);
+        });
+
+        delay(3000, () => {
+          setClassName("answer");
+          setTimeOver(true);
+        });
+      }
+    });
+  };
+  return (
+    <div className="Game">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => !selectedAnswer && handleClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
